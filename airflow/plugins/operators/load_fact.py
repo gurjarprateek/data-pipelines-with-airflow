24a8fafe515dc460d81bf097940085f5cffa467e
Prateek Gurjar <prateek.gurjar@vecna.com>
2019-08-06 21:59:29 -0400

    load fact operator

---
+++ airflow/plugins/operators/load_fact.py
@@ -0,0 +1,30 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadFactOperator(BaseOperator):
+    
+    ui_color = '#80BD9E'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 table="",
+                 sql_template="",
+                 *args, **kwargs):
+
+        super(LoadFactOperator, self).__init__(*args, **kwargs)
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.sql_template = sql_template
+
+    def execute(self, context):
+
+      try:
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+      except Exception as error:
+        print("set redshift connection in airflow connections")
+        raise(error)
+      
+      self.log.info("Loading data in the dimension table")
+      redshift.run("INSERT INTO {0}\n{1}".format(self.table, self.sql_template))
